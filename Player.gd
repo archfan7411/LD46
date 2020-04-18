32b4de7b2f1cbccde8308cd7e670509f95de835a
archfan <33993466+archfan7411@users.noreply.github.com>
2020-04-18 17:29:29 -0400

    Remove up/down movement

--- Player.gd
+++ Player.gd
@@ -4,11 +4,7 @@ extends KinematicBody2D
 var speed = 3000
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("up"):
-		self.move_and_slide(Vector2(0, -speed))
-	elif Input.is_action_just_pressed("down"):
-		self.move_and_slide(Vector2(0, speed))
-	elif Input.is_action_just_pressed("left"):
+	if Input.is_action_just_pressed("left"):
 		self.move_and_slide(Vector2(-speed, 0))
 	elif Input.is_action_just_pressed("right"):
 		self.move_and_slide(Vector2(speed, 0))
