e6cd47de5a4c336bf5dc639682430b6cbae4c246
archfan <33993466+archfan7411@users.noreply.github.com>
2020-04-18 19:34:41 -0400

    WIP scene loading fixes

--- Level.gd
+++ Level.gd
@@ -1,3 +1,5 @@
+extends Node2D
+
 var map_desc = {
 	" " : preload("res://Obstacle.tscn"),
 	"." : preload("res://Obstacle.tscn"),
@@ -5,22 +7,23 @@ var map_desc = {
 	"G" : preload("res://Obstacle.tscn"),
 	"P" : preload("res://Obstacle.tscn"),
 	"I" : preload("res://Obstacle.tscn"),
+	"#" : preload("res://Obstacle.tscn"),
 }
 
 func load_level_file(filename):
 	var lines = []
 	var filestream = File.new()
-	filestream.open("res://levels/" + filename)
-	while not filestream.end_of_file():
+	filestream.open("res://levels/" + filename, File.READ)
+	while not filestream.eof_reached():
 		lines.append(filestream.get_line())
 	filestream.close()
 	for y in range(len(lines)):
 		for x in range(len(lines[0])):
 			var tile_str = lines[y][x]
 			var new_instance = map_desc[tile_str].instance()
-			new_instance.Translation = Vector2(x * 3000, y * 3000)
-			self.get_root.add_child(new_instance)
+			new_instance.global_position = Vector2(x * 3000, y * 3000)
+			self.add_child(new_instance)
 
 # This doesn't seem to work, please fix.
-func _on_Level_ready():
+func _ready():
 	load_level_file("test_level_2.txt")
