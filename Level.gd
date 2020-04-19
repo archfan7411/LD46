364fc7ea12aba2c4a8133caba8eeead4f511f650
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2020-04-19 12:10:17 -0500

    Add basic state, currently not working due to bad type checking.

--- Level.gd
+++ Level.gd
@@ -3,6 +3,10 @@ extends Node2D
 const SPACE = 50
 const BUFFER = 10
 
+var state = null
+var State = load("State.gd")
+var Player = load("Player.gd")
+
 var map_desc = {
 	" " : preload("res://Empty.tscn"), # empty
 	"." : preload("res://Dirt.tscn"), # dirt
@@ -20,13 +24,19 @@ func load_level_file(filename):
 	while not filestream.eof_reached():
 		lines.append(filestream.get_line())
 	filestream.close()
+	var lvl_map = []
 	for y in range(len(lines)):
+		var row = []
 		for x in range(len(lines[0])):
 			var tile_str = lines[y][x]
 			var new_instance = map_desc[tile_str].instance()
 			new_instance.global_position = Vector2(x * SPACE, y * SPACE)
 			self.add_child(new_instance)
-	OS.window_size = Vector2((len(lines)*SPACE)-BUFFER, (len(lines[0])*SPACE)-BUFFER)
+			row.append(new_instance)
+		lvl_map.append(row)
+	var plr = Player.new()
+	state = State.new(lvl_map, plr)
+    OS.window_size = Vector2((len(lines)*SPACE)-BUFFER, (len(lines[0])*SPACE)-BUFFER)
 
 # This doesn't seem to work, please fix.
 func _ready():
--- Player.gd
+++ Player.gd
@@ -1,14 +1,7 @@
 extends KinematicBody2D
 
-
 var speed = 50
 
-func _physics_process(delta):
-	if Input.is_action_just_pressed("up"):
-		self.translate(Vector2(0, -speed))
-	elif Input.is_action_just_pressed("down"):
-		self.translate(Vector2(0, speed))
-	elif Input.is_action_just_pressed("left"):
-		self.translate(Vector2(-speed, 0))
-	elif Input.is_action_just_pressed("right"):
-		self.translate(Vector2(speed, 0))
+func _physics_process(_delta):
+	var state = get_parent().state
+	state.move()
